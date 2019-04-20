12e2f114aa7b5b1d0db15dff57a85beb96b005d7
Leo <leonardo.f.jr@gmail.com>
2019-04-20 17:10:15 -0400

    /

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { $ } from 'protractor';
 declare var FB: any;
 
 @Component({
@@ -8,6 +9,7 @@ declare var FB: any;
 })
 export class AppComponent implements OnInit {
   title = 'Hangman';
+  loginStatus:boolean = false;
   ngOnInit() {
 
     (window as any).fbAsyncInit = function () {
@@ -30,12 +32,16 @@ export class AppComponent implements OnInit {
 
   }
 
-  submitLogin() {
+  submitLogin(el) {
     console.log("submit login to facebook");
     // FB.login();
     FB.login((response) => {
       console.log('submitLogin', response);
       if (response.authResponse) {
+        this.hideLoginBtn();
+        this.showLogoutBtn();
+        console.log(el);
+        this.loginStatus = true;
         this.graphAPI(response.authResponse.userID);
         //login success
         //login success code here
@@ -47,12 +53,34 @@ export class AppComponent implements OnInit {
     });
 
   }
+  hideLogoutBtn() {
+    document.getElementById('logoutBtn').style.display = "none";
 
+  }
+  showLogoutBtn() {
+    document.getElementById('logoutBtn').style.display = "block";
+
+  }
+  showLoginBtn() {
+    document.getElementById('loginBtn').style.display = "block";
+  }
+  hideLoginBtn() {
+    document.getElementById('loginBtn').style.display = "none";
+  }
   logout() {
+    let _self = this;
     FB.logout(function(response) {
-      console.log('logout', response)
+      if (response) {
+        _self.loginStatus = false;
+        _self.showLoginBtn();
+        _self.hideLogoutBtn();
+        console.log('logout', response)
+      }
+
     })
   }
+
+
 
   graphAPI(userId) {
     /* make the API call */
